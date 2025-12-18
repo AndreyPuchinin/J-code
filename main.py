84ed77f1b64b7086ae3e6eedf26eb99a1558f5a0
AndyKybik <78998186+AndreyPuchinin@users.noreply.github.com>
2025-12-18 04:33:52 +0000

    причесал вывод (разделил строки-деревья команд)

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ linker = LinkerAndSyntaxChecker(code)
 # Вводим J-карту команд
 cmd_J_map = linker.generate_cmd_J_map()
 print(cmd_J_map)
+print()
 
 cmd_J_map_with_parents = linker.generate_cmd_J_map_with_parents(cmd_J_map)
 print(cmd_J_map_with_parents)
+print()
 
 # Преобразуем JSON-дерево в дерево объектов команд
 cmd_obj_map = linker.generate_cmd_obj_map(cmd_J_map_with_parents)
 print("!!!", cmd_obj_map)
+print()
 
 # Проверяем все команды на ошибки
 linker.validate_command_tree(cmd_obj_map)
